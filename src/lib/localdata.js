0e25143353831cc696d274724b22da70a5cc4d3b
younes-namipoor <yonesnami2@gmail.com>
2024-12-31 16:22:42 +0330

    any files and folders

--- src/lib/localdata.js
+++ src/lib/localdata.js
@@ -1,8 +1,8 @@
+
 import fsPromises from "fs/promises";
 import path from "path";
-
 export async function getLocalData() {
-  const filePath = path.join(process.cwd(), "./json/data.json");
+  const filePath = path.join(process.cwd(), "./src/json/data.json");
   const jsonData = await fsPromises.readFile(filePath);
   const objectData = JSON.parse(jsonData);
 
